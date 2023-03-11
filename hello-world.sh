7c2715e7d641ddfbb51acfc0f3ea2c1b9d45ed18
subAngel <whoangel.agl@gmail.com>
2023-03-11 13:05:54 -0600

    Delimitando salidas con el comando cat

--- hello-world.sh
+++ hello-world.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# este es un comentario
 cat >> hola.txt
+
+: 'este es un comentario
+ de dos lineas y puede llegar a 
+ser un comentario mas largo'
 
 
---
+++ output.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cat << myText
+esto es un texto
+que se mostrara 
+por la terminal
+myText
