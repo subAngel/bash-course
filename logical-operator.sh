30d2c5da5954b345caf7341e4041bce6b2568d6b
subAngel <whoangel.agl@gmail.com>
2023-03-12 17:59:24 -0600

    Operadores logicos

--- logical-operator.sh
+++ logical-operator.sh
@@ -3,8 +3,10 @@
 age=33
 
 # 18 > age < 40
-#if [ $age -gt 18 ] && [ $age -lt 40 ]
-if [[ $age -gt 18 || $age -lt 40 ]]
+# if [ $age -gt 18 ] && [ $age -lt 40 ]
+# if [[ $age -gt 18 || $age -lt 40 ]]
+# or
+if [ $age -gt 18 -o $age -lt 40 ]
 then 
     echo "Edad valida"
 else
